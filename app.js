8a6780bbe75d767abc3ef42e221cdd0b968412d7
BuhleMtshali <mtshalizanobuhle7@gmail.com>
2024-07-03 23:07:38 +0200

    updated the index.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+function giveRating(event) {
+  event.preventDefault();
+  let innerContainer = document.querySelector("#inner");
+  let ratingNumbert = document.querySelector("#rating");
+  let hiddenElement = document.querySelector("#hidden-part").innerHTML;
+  innerContainer.innerHTML = hiddenElement;
+}
 
+let submitButton = document.querySelector("#submit");
+submitButton.addEventListener("click", giveRating);
